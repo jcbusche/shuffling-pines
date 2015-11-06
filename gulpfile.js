248f60a6b02832007c4507f834733926c1daf6b4
john <jcbusche@gmail.com>
2015-11-06 11:45:28 -0500

    first commit; basics of form working but need to put data handling in a service

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,22 @@
+"use strict";
+
+//Include gulp
 var gulp = require('gulp');
+
+//include plugins
+var jshint = require('gulp-jshint');
+
+//lint task
+gulp.task('lint', function(){
+	return gulp.src('src/**/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('src/**/*.js', ['lint']);
+});
+
+
+//default
+gulp.task('default', ['lint']);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
-var app = angular.module('shuffling', []);
+/*global angular */
+angular.module('shuffling', [])
+	.controller('FormController', [function(){
+		this.guests = [];
+		var myStorage;
 
-app.controller('FormController', [function(){
+		this.addguest = function(){
+			if (this.name === null || this.date === null || this.status === null){
+				alert("You must fill out all fields of the form");
+			}
+			else {
+				newGuest = {name: this.name, date: this.date, status: this.status, location: this.location};
+				guestKey = this.name;
+				this.name = '';
+				this.date = '';
+				this.status = '';
+				this.location = '';
+				guest = JSON.stringify(newGuest);
+				localStorage.setItem(guestKey, guest);
+				guestNum = 0;
+				for (var key in localStorage){
+					console.log("Guest " + guestNum + ": " + key + localStorage[key]);
+					guestNum++;
+				}
+			}
+		};
 
+	}])
+	.controller('TabController', [function(){
 
-}]);
-
-app.controller('TabController', [function(){
-
-}]);
+	}]);
+	//.factory('addguest', )
