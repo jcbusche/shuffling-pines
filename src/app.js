d603a3c7399d364afa6d8b72421ef42c2ce5527c
john <jcbusche@gmail.com>
2015-11-06 14:15:03 -0500

    updated tabcontroller

--- src/app.js
+++ src/app.js
@@ -1,32 +1,63 @@
 /*global angular */
 angular.module('shuffling', [])
-	.controller('FormController', [function(){
-		this.guests = [];
-		var myStorage;
+	.controller('FormController', ['guestStorage', 'TabController', function(guestStorage, TabController){
 
-		this.addguest = function(){
-			if (this.name === null || this.date === null || this.status === null){
-				alert("You must fill out all fields of the form");
+		this.storeGuest = function(){
+			//Check to see the form is completely filled out
+			if (this.name === null || this. date === null || this.satus === null || this.location ===null){
+				alert("Please complete the form before submitting.");
+				//Not clearing form here because that could potentially be very annoying.
+			}
+			//Check to see if someone is trying to register a guest that already exists
+			else if (localStorage.getItem(this.name) !== null){
+				alert("It seems that this guest is already registered; please edit it from the \"Guests\" tab.");
 			}
 			else {
-				newGuest = {name: this.name, date: this.date, status: this.status, location: this.location};
-				guestKey = this.name;
+				guestStorage.storeGuest(this.name, this.date, this.status, this.location);
+
 				this.name = '';
 				this.date = '';
 				this.status = '';
 				this.location = '';
-				guest = JSON.stringify(newGuest);
-				localStorage.setItem(guestKey, guest);
 				guestNum = 0;
 				for (var key in localStorage){
 					console.log("Guest " + guestNum + ": " + key + localStorage[key]);
 					guestNum++;
+					//Only enable below to clear localStorage
+					//localStorage.removeItem(key);
 				}
+				TabController.setTab(2);
 			}
 		};
-
 	}])
 	.controller('TabController', [function(){
 
+		this.tab = 1;
+
+		this.setTab = function(num){
+			this.tab = num;
+			console.log("tab = " + num);
+		};
+		this.checkTab = function(num){
+			return this.tab === num;
+		};
+
+	}])
+	.controller('ListController', [function(){
+		guestList = localStorage;
+	}])
+	.service('createGuest', [function(){
+		this.addGuest = function(name, date, status, location){
+			console.log("createGuest called");
+			newGuest = {name: name, date: date, status: status, location: location};
+			return newGuest;
+		};
+	}])
+	.service('guestStorage', ['createGuest', function(createGuest){
+		this.storeGuest = function(name, date, status, loc){
+			console.log('guestStorage called');
+			guestToStore = createGuest.addGuest(name, date, status, loc);
+			stringGuest = JSON.stringify(guestToStore);
+			localStorage.setItem(name, stringGuest);
+		};
 	}]);
-	//.factory('addguest', )
